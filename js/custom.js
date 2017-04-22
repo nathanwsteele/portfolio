359256b9f5c4dca6e073c2d6880e48817c39e964
STEELE <nathansteele@northwesternmutual.com>
2017-04-22 05:49:25 -0500

    First commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+     var startingPos = 0; /* << This is the value you need to change in order to position the parallax image. The number represents the pixel value of the margin-top. */
+   /* $('#home').css('background-position','50% ' + startingPos + 'px');*/
+    
+    $(window).scroll(function(){    
+        var scrl_tp = $(this).scrollTop();
+        /*$('#home').css('background-position','50% ' + (parseInt(-scrl_tp/4) + startingPos)+ 'px');*/
+    });     
+    
+    /*  Smooth scrolling nav ...........................................  */
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').on('click', function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                /* Finding the page offset height and offsetting it by the height of the banner so
+                  that the page scrolls to the correct place, and the banner doesn't cover the header. */
+                var customHeight = (target.offset().top)/* - ($("#customBanner").height() - 5);*/
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: customHeight
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+});//End Ready
+
